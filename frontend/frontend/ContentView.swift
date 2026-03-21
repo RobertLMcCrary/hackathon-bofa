3126f26bcf2a6f91649230ee821c40e0210c5480
btranho1402 <101420022+btranho1402@users.noreply.github.com>
2026-03-21 11:36:35 -0400

    discover screen and UI testing

--- frontend/frontend/ContentView.swift
+++ frontend/frontend/ContentView.swift
@@ -1,23 +1,7 @@
 import SwiftUI
-import Supabase
 
 struct ContentView: View {
-    @State var isAuthenticated = false
-    
     var body: some View {
-        Group {
-            if isAuthenticated {
-                ProfileView()
-            } else {
-                AuthView()
-            }
-        }
-        .task {
-            for await state in supabase.auth.authStateChanges {
-                if [.initialSession, .signedIn, .signedOut].contains(state.event) {
-                    isAuthenticated = state.session != nil
-                }
-            }
-        }
+        DiscoverView()
     }
 }
